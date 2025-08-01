5ee6b3ccfbccf9f5bd5c9da1c40db4c54a30db88
N'GANNA <mondesirngannakone@email.com>
2025-08-01 17:44:56 +0000

    Ajout du fichier index.html

---
+++ dashborad/header.js
@@ -0,0 +1,13 @@
+﻿    fetch('header.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+    return response.text();
+    })
+    .then(html => {
+        document.getElementById('header-container').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Erreur de chargement de la header:', error);
+    });
